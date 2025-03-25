e3d3dee5ca175217e50ba4b71d492aed6ac9d1d2
Nash504 <nashdfern@gmail.com>
2025-03-25 12:02:35 +0530

    hover

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,10 +6,11 @@ export default function ProjectCard({ title, onClick }) {
     <Card
       onClick={onClick}
       className="border shadow bg-transparent p-6 w-full max-w-[400px] cursor-pointer 
-                 transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg"
+                 transition-all duration-700 ease-[cubic-bezier(0.25,1,0.5,1)] 
+                 hover:scale-105 hover:shadow-2xl will-change-transform "
     >
       <div className="flex flex-col items-center">
-        <div className="relative w-full h-[120px] mt-4 rounded-lg overflow-hidden bg-gray-100">
+        <div className="relative w-full h-[100px] mt-4 rounded-lg overflow-hidden bg-gray-100">
           <Image src="/window.svg" fill className="p-3" alt={title} />
         </div>
         <CardTitle className="justify-center p-8 text-2xl font-bold mt-2 -mb-8">
